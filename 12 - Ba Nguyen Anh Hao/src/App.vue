2002a9698e1d6cafdd096648ac34fcd00886485f
Hao Nguyen <haobnats00832@fpt.edu.vn>
2026-01-21 20:32:40 +0700

    Hoan thanh Bai 1 lab3

--- 12 - Ba Nguyen Anh Hao/src/App.vue
+++ 12 - Ba Nguyen Anh Hao/src/App.vue
@@ -1,16 +1,18 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Trang34 from './components/Trang34.vue';
 </script>
 
 <template>
-  <div>
+  <Trang34/>
+  <!-- <div>
     <a href="https://vite.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
-  </div>
+  </div> -->
   
   <HelloWorld msg="12- Ba Nguyễn Anh Hào" />
 
