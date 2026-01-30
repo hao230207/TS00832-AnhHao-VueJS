7d9b13c50677e41fd73f1c1bd45c75c714079ff5
Hao Nguyen <haobnats00832@fpt.edu.vn>
2026-01-30 17:59:39 +0700

    Hoàn thành Bai 3 Lab5

--- Lab5/src/App.vue
+++ Lab5/src/App.vue
@@ -3,11 +3,12 @@
     <h1 class="text-center mt-3">Lab 5 - VueJS</h1>
     <hr>
     
-    <bai2 />
+    <bai3 />
     </div>
 </template>
 
 <script setup>
 import bai1 from './components/bai1.vue';
 import bai2 from './components/bai2.vue';
+import bai3 from './components/bai3.vue';
 </script>
