778ca5704ed71db6af57b4c92e343c62f027f731
Ba Nguyen Anh Hao <haobnats00832@fpt.edu.vn>
2026-02-27 16:20:19 +0700

    Hoàn thành bài 4 lab 7

--- Lab7/src/App.vue
+++ Lab7/src/App.vue
@@ -2,6 +2,7 @@
 import Bai1 from './components/Bai1.vue';
 import Bai2 from './components/Bai2.vue';
 import Bai3 from './components/Bai3.vue'
+import Bai4 from './components/Bai4.vue'
 </script>
 
 <template>
@@ -10,7 +11,7 @@ import Bai3 from './components/Bai3.vue'
     
 
    
-    <Bai3 />
+    <Bai4 />
 
 
 </template>
