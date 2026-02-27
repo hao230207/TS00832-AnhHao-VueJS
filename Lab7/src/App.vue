8b8ef2fa606fe6c938105de3daf1c973176bce1f
Ba Nguyen Anh Hao <haobnats00832@fpt.edu.vn>
2026-02-27 15:17:32 +0700

    Hoàn thành Bài 1 Lab 7

---
+++ Lab7/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Bai1 from './components/Bai1.vue'
+
+</script>
+
+<template>
+  
+    <h2>12-Ba Nguyễn Anh Hào - MSSV: TS00832</h2>
+    
+
+   
+    <Bai1 />
+
+
+</template>
